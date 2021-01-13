471bbc468dc46ef6fef1c6b7cc477a1b3944ed77
zhangminghui6106 <zhangminghui6106@navinfo.com>
2021-01-13 20:34:46 +0800

    build.sh特殊处理readme.md

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ SVC_DIR="${ROOT_DIR}/svc"
 cd $SVC_DIR
 docker login -u itinfomation -p Zmh920226docker!
 for svc in *; do
+    if [[ -f README.md]]; then
+        continue
+    fi
     cd "${SVC_DIR}/$svc"
     if [[ ! -f Dockerfile ]]; then
         continue
